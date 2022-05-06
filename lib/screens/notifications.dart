0f2bb5940c969127e2ed8463de3544bc9f0be179
peter <patourtohme9@gmail.com>
2022-05-06 16:33:45 +0300

    fixed order id in notification widget

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -36,85 +36,90 @@ class _NotificationsState extends State<Notifications> {
     final order = Provider.of<OrderProvider>(context, listen: true);
 
     return Scaffold(
-      body: noti.loading?Center(child: CircularProgressIndicator()):user.status == Status.isVerified
-          ? ListView.builder(
-          itemCount: noti.services.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Container(
-                height: 100,
-                child: InkWell(
-                  onTap: () {
-                    print("ORDER  ID: ${noti.services[index].orderId}");
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) =>
-
-                            TabBarOrder(0, noti.services[index].orderId),
+      body: noti.loading
+          ? Center(child: CircularProgressIndicator())
+          : user.status == Status.isVerified
+              ? ListView.builder(
+                  itemCount: noti.services.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        height: 100,
+                        child: InkWell(
+                          onTap: () {
+                            print("ORDER  ID: ${noti.services[index].orderId}");
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => TabBarOrder(
+                                    0, noti.services[index].orderId),
+                              ),
+                            );
+                          },
+                          child: Card(
+                              child: ListTile(
+                            trailing: Text(
+                              "${DateFormat.yMd().add_jm().format(
+                                    DateTime.parse(
+                                        noti.services[index].createdDate),
+                                  )}",
+                              style: TextStyle(
+                                  fontSize: 11, color: Colors.black54),
+                            ),
+                            leading: CircleAvatar(
+                              foregroundImage: AssetImage(
+                                "assets/images/motornew.png",
+                              ),
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.red,
+                              radius: 20,
+                            ),
+                            title: Text(
+                              "${noti.services[index].title}",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: yellowColor,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              '${noti.services[index].description}',
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.black87),
+                            ),
+                          )),
+                        ),
                       ),
                     );
-                  },
-                  child: Card(
-                      child: ListTile(
-                        trailing: Text(
-                          "${DateFormat.yMd().add_jm().format(
-                            DateTime.parse(
-                                noti.services[index].createdDate),
-                          )}",
-                          style: TextStyle(fontSize: 11, color: Colors.black54),
+                  })
+              : Center(
+                  child: TextButton(
+                    onPressed: () async {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignInScreen()));
+                    },
+                    child: const Text('Sign In',
+                        style: TextStyle(
+                          color: yellowColor,
+                          fontFamily: 'BerlinSansFB',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        )),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: MediaQuery.of(context).size.height * 0.035,
+                          horizontal: 30),
+                      side: const BorderSide(color: Colors.black, width: 1),
+                      backgroundColor: const Color(0xFF333333),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
                         ),
-                        leading: CircleAvatar(
-                          foregroundImage: AssetImage(
-                            "assets/images/motornew.png",
-                          ),
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.red,
-                          radius: 20,
-                        ),
-                        title: Text(
-                          "${noti.services[index].title}",
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: yellowColor,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          '${noti.services[index].description}',
-                          style: TextStyle(fontSize: 12, color: Colors.black87),
-                        ),
-                      )),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            );
-          })
-          : Center(
-        child: TextButton(
-          onPressed: () async {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => SignInScreen()));
-          },
-          child: const Text('Sign In',
-              style: TextStyle(
-                color: yellowColor,
-                fontFamily: 'BerlinSansFB',
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-              )),
-          style: TextButton.styleFrom(
-            padding: EdgeInsets.symmetric(
-                vertical: MediaQuery.of(context).size.height * 0.035,
-                horizontal: 30),
-            side: const BorderSide(color: Colors.black, width: 1),
-            backgroundColor: const Color(0xFF333333),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
