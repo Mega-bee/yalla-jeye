af74ecaa7a80fc7ad9b26ba07c091803d0422d81
peter <patourtohme9@gmail.com>
2022-05-09 20:07:16 +0300

    Fixed notifications

--- lib/Services/local_notifications.dart
+++ lib/Services/local_notifications.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/material.dart';
@@ -6,57 +8,58 @@ import 'package:yallajeye/screens/order/tabbar_order.dart';
 
 class LocalNotificationService {
   static final FlutterLocalNotificationsPlugin _notificationPlugin =
-  FlutterLocalNotificationsPlugin();
+      FlutterLocalNotificationsPlugin();
 
   static void initialize(BuildContext context) {
     final InitializationSettings initializationSettings =
-    InitializationSettings(
-        android: AndroidInitializationSettings("@mipmap/ic_launcher"));
+        InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+    );
+    const IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(requestSoundPermission: true);
+    final InitializationSettings initializationSettingss =
+        InitializationSettings(
+      iOS: initializationSettingsIOS,
+    );
 
     _notificationPlugin.initialize(initializationSettings,
-        onSelectNotification: (String payloadRoute) async {
-          // Navigator.pushNamed(context, payloadRoute);
-          // Navigator.pushNamedAndRemoveUntil(
-          //     context, payloadRoute, (route) => false);
-          // demo data
-          Navigator.of(context).push(MaterialPageRoute(builder: (_)=>TabBarOrder(0, 333)));
-        });
+        onSelectNotification: (String payload) async {
+      print("Payload:${payload}");
+      if (payload != null) {
+        var message = jsonDecode(payload);
+        print("decode 1");
+        print(message);
+        int id = int.parse(message["orderId"].toString());
+        print("id is: ${id.toString()}");
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (_) => TabBarOrder(numTab: 0, id: id)));
+      }
+    });
   }
 
   static Future<void> display(RemoteMessage message) async {
+    // print("RemoteMssg: ${json.encode(message)}");
     try {
       final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
       final NotificationDetails notificationDetails = NotificationDetails(
           android: AndroidNotificationDetails(
-            "easyapproach",
-            "easyapproach channel",
-            channelDescription: "this is our channel",
-            importance: Importance.max,
-            priority: Priority.high,
-          ));
+        "easyapproach",
+        "easyapproach channel",
+        channelDescription: "this is our channel",
+        importance: Importance.max,
+        priority: Priority.high,
+        showWhen: true,
+        playSound: true,
+        channelShowBadge: true,
+        enableLights: true,
+        enableVibration: true,
+        onlyAlertOnce: false,
+      ));
       await _notificationPlugin.show(id, message.notification.title,
           message.notification.body, notificationDetails,
-          payload: 'routePage'
-      );
+          payload: json.encode(message.data));
     } on Exception catch (e) {
       print(e);
     }
   }
-
-
-
-
-
 }
-
-
-// class fcmId{
-//   static Future<void> setFcmId(String fcmId) async {
-//     var pref = await SharedPreferences.getInstance();
-//     await pref.setString(fcmId, fcmId);
-//   }
-//
-//   static Future<String> getFcmId(String fcmId) async {
-//     return (await SharedPreferences.getInstance()).getString(fcmId) ?? null;
-//   }
-// }
